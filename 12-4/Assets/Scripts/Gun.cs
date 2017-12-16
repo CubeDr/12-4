6c81bcf448262e073f9faeeb8a184f753f8326f0
CubeDr <spaceship00@naver.com>
2017-12-16 20:56:09 +0900

    Player bullet

--- 12-4/Assets/Scripts/Gun.cs
+++ 12-4/Assets/Scripts/Gun.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Gun : MonoBehaviour {
+    public GameObject target;
+    public GameObject bulletPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
 	}
+
+    private void Shoot() {
+        GameObject bulletObject = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        bulletObject.GetComponent<Bullet>().target = target.transform.position;
+    }
 }
