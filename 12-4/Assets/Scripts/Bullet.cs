8e27e8d1d98ff75e1d76b8d0c64e6aec3a7d658d
CubeDr <spaceship00@naver.com>
2017-12-16 20:40:12 +0900

    Create bullet script

--- 12-4/Assets/Scripts/Bullet.cs
+++ 12-4/Assets/Scripts/Bullet.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bullet : MonoBehaviour {
+    private Vector3 _target;
+    private Vector3 direction; // 단워 벡터
+    public Vector3 target {
+        get {
+            return _target;
+        }
+        set {
+            _target = value;
+            direction = (_target - transform.position).normalized;
+            transform.LookAt(_target);
+        }
+    }
+    public float speed = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.position += direction * speed * Time.deltaTime;
 	}
 }
