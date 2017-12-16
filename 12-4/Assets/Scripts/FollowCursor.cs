e850eb591ddb10a0da732ddacc304b017130c126
CubeDr <spaceship00@naver.com>
2017-12-16 20:30:29 +0900

    player follows cursor

---
+++ 12-4/Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class FollowCursor : MonoBehaviour {
+	void Update () {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        transform.position = pos;
+    }
+}
